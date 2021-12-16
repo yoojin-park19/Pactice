e6839542368ee00aa654b7ad8cc9d86d06f3a744
박유진(Park Yoojin) <93498523+yoojin-park19@users.noreply.github.com>
2021-12-16 23:56:17 +0900

    update button function

--- vending-machine/vending-machine_1/resource/js/machine.js
+++ vending-machine/vending-machine_1/resource/js/machine.js
@@ -1,1 +1,10 @@
-document.getElementById('helloworld').innerHTML = 'hello world';
+let btnPut = document.getElementById('btnPut');
+let moneyPut = document.getElementById('moneyPut').value;
+let moneyIn = parseInt(moneyPut);
+
+btnPut.addEventListener('click', function addMoney(){
+    let totalMoney = 25000;
+    totalMoney = totalMoney + moneyIn;
+    document.getElementById('totalMoney').innerHTML = totalMoney + '원';
+});
+
