923d6225edf14de44055c78752c14419d05f32e0
박유진(Park Yoojin) <93498523+yoojin-park19@users.noreply.github.com>
2021-12-17 17:30:14 +0900

    선택 기능 업로드

--- vending-machine/vending-machine_1/resource/js/machine.js
+++ vending-machine/vending-machine_1/resource/js/machine.js
@@ -1,10 +1,74 @@
-let btnPut = document.getElementById('btnPut');
-let moneyPut = document.getElementById('moneyPut').value;
-let moneyIn = parseInt(moneyPut);
+let btnOrigin = document.getElementById('btn_origin');
+let countOrigin =0;
 
-btnPut.addEventListener('click', function addMoney(){
-    let totalMoney = 25000;
-    totalMoney = totalMoney + moneyIn;
-    document.getElementById('totalMoney').innerHTML = totalMoney + '원';
-});
+btnOrigin.addEventListener('click', function add() {
+    if (countOrigin == 0) {
+        $('.staged-origin').css('display','block');
+        countOrigin += 1;
+    }
+    else {
+        countOrigin += 1;
+    }
+    document.getElementById('count-origin').innerText = countOrigin;
+    document.getElementById('count-origin1').innerText = countOrigin;
+})
 
+let btnYellow = document.getElementById('btn_yellow');
+let countYellow =0;
+
+btnYellow.addEventListener('click', function addYellow() {
+    if (countYellow == 0) {
+        $('.staged-yellow').css('display','block');
+        countYellow += 1;
+    }
+    else {
+        countYellow += 1;
+    }
+    document.getElementById('count-yellow').innerText = countYellow;
+    document.getElementById('count-yellow1').innerText = countYellow;
+})
+
+let btnCool = document.getElementById('btn_cool');
+let countCool =0;
+
+btnCool.addEventListener('click', function addCool() {
+    if (countCool == 0) {
+        $('.staged-cool').css('display','block');
+        countCool += 1;
+    }
+    else {
+        countCool += 1;
+    }
+    document.getElementById('count-cool').innerText = countCool;
+    document.getElementById('count-cool1').innerText = countCool;
+})
+
+let btnGreen = document.getElementById('btn_green');
+let countGreen =0;
+
+btnGreen.addEventListener('click', function addGreen() {
+    if (countGreen == 0) {
+        $('.staged-green').css('display','block');
+        countGreen += 1;
+    }
+    else {
+        countGreen += 1;
+    }
+    document.getElementById('count-green').innerText = countGreen;
+    document.getElementById('count-green1').innerText = countGreen;
+})
+
+let btnOrange = document.getElementById('btn_orange');
+let countOrange =0;
+
+btnOrange.addEventListener('click', function addOrange() {
+    if (countOrange == 0) {
+        $('.staged-orange').css('display','block');
+        countOrange += 1;
+    }
+    else {
+        countOrange += 1;
+    }
+    document.getElementById('count-orange').innerText = countOrange;
+    document.getElementById('count-orange1').innerText = countOrange;
+})
